62cb2916fe223d79167241343bf4a044b9887970
Michael Accardo <accardo@cpan.org>
2009-05-03 23:02:35 +0000

    Import of ACCARDO/HTML-Perlinfo-1.61 from CPAN.
    gitpan-cpan-distribution: HTML-Perlinfo
    gitpan-cpan-version:      1.61
    gitpan-cpan-path:         ACCARDO/HTML-Perlinfo-1.61.tar.gz
    gitpan-cpan-author:       ACCARDO
    gitpan-cpan-maturity:     released

--- lib/HTML/Perlinfo.pm
+++ lib/HTML/Perlinfo.pm
@@ -12,7 +12,7 @@ use HTML::Perlinfo::Common;
 use base qw(Exporter HTML::Perlinfo::Base);
 our @EXPORT = qw(perlinfo);
 
-our $VERSION = '1.60';
+our $VERSION = '1.61';
 
 sub perlinfo {
   my ($opt) = @_;
